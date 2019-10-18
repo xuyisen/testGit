f2cf5326489742a32b261c398dba6097fbfd88d4
xuyisen <18071748665@163.com>
2019-10-18 20:23:24 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void A(){
+
+    }
+
+    public static void B(){
+
+    }
+
+    public static int A(int a){
+        return 0;
+    }
+
+    public static void B(int b){
+
+    }
+}
